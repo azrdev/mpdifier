2c1d9a8b6b4a1c38dbf23630f4fe5aecce7dd479
jBiegert <azrdev@qrdn.de>
2013-08-22 23:51:12 +0200

    native WinAPI usage

--- mpdifier.py
+++ mpdifier.py
@@ -3,55 +3,113 @@
 import subprocess
 import mpdserver
 
-""" pressing media keys, using external C binary """
+""" pressing media keys using WinAPI """
+#code from http://code.google.com/p/pywinauto/
+import ctypes
+from ctypes import *
+
+WORD = ctypes.c_ushort
+DWORD = ctypes.c_ulong
+
+class KEYBDINPUT(ctypes.Structure):
+    _pack_ = 2
+    _fields_ = [
+        ('wVk', WORD),
+        ('wScan', WORD),
+        ('dwFlags', DWORD),
+        ('time', DWORD),
+        ('dwExtraInfo', DWORD),
+    ]
+
+#dummys, don't need those structures here
+class MOUSEINPUT(ctypes.Structure):
+    pass
+
+class HARDWAREINPUT(ctypes.Structure):
+    pass
+
+class UNION_INPUT_STRUCTS(ctypes.Union):
+    _fields_ = [
+        ('mi', MOUSEINPUT),
+        ('ki', KEYBDINPUT),
+        ('hi', HARDWAREINPUT),
+    ]
+
+class INPUT(ctypes.Structure):
+    _pack_ = 2
+    _fields_ = [
+        ('type', DWORD),
+        ('_', UNION_INPUT_STRUCTS),
+    ]
+
+INPUT_KEYBOARD = 1
+KEYEVENTF_KEYUP = 2
+
+""" param  key - the key to press, must be a WORD """
 def pressKey(key):
-    subprocess.call(['sendMediaKeys.exe', key])
+    inp = INPUT()
+    inp.type = INPUT_KEYBOARD
+    inp._.ki.wScan = 0
+    inp._.ki.time = 0
+    inp._.ki.dwExtraInfo = 0
+    inp._.ki.wVk = key
+    inp._.ki.dwFlags = 0
 
-class Key:
-   VolumeUp = "+"
-   VolumeDown = "-"
-   Mute = "m"
+    ctypes.windll.user32.SendInput(1,
+                                   ctypes.pointer(inp),
+                                   ctypes.sizeof(inp))
 
-   TogglePause = "t"
-   Next = "n"
-   Previous = "p"
-   Stop = "s"
+    inp._.ki.dwFlags = KEYEVENTF_KEYUP
+
+    ctypes.windll.user32.SendInput(1,
+                                   ctypes.pointer(inp),
+                                   ctypes.sizeof(inp))
+
+
+class Key: # enum
+    VK_VOLUME_MUTE = 0xAD
+    VK_VOLUME_DOWN = 0xAE
+    VK_VOLUME_UP = 0xAF
+    VK_MEDIA_NEXT_TRACK = 0xB0
+    VK_MEDIA_PREV_TRACK = 0xB1
+    VK_MEDIA_STOP = 0xB2
+    VK_MEDIA_PLAY_PAUSE = 0xB3
 
 
 # command classes to be registered to mpd
 
 class VolumeUp(mpdserver.Command):
     def handle_args(self):
-        pressKey(Key.VolumeUp)
+        pressKey(Key.VK_VOLUME_UP)
 
 class VolumeDown(mpdserver.Command):
     def handle_args(self):
-        pressKey(Key.VolumeDown)
+        pressKey(Key.VK_VOLUME_DOWN)
 
 class Mute(mpdserver.Command):
     def handle_args(self):
-        pressKey(Key.Mute)
+        pressKey(Key.VK_VOLUME_MUTE)
 
 
 class Play(mpdserver.Command):
     def handle_args(self):
-        pressKey(Key.TogglePause)
+        pressKey(Key.VK_MEDIA_PLAY_PAUSE)
 
 class Pause(mpdserver.Command):
     def handle_args(self):
-        pressKey(Key.TogglePause)
+        pressKey(Key.VK_MEDIA_PLAY_PAUSE)
 
 class Next(mpdserver.Command):
     def handle_args(self):
-        pressKey(Key.Next)
+        pressKey(Key.VK_MEDIA_NEXT_TRACK)
 
 class Previous(mpdserver.Command):
     def handle_args(self):
-        pressKey(Key.Previous)
+        pressKey(Key.VK_MEDIA_PREV_TRACK)
 
 class Stop(mpdserver.Command):
     def handle_args(self):
-        pressKey(Key.Stop)
+        pressKey(Key.VK_MEDIA_STOP)
 
 
 """ setup & usage of mpdserver """
