25b83788b6e00e43430a4770766cd309157ca212
jBiegert <azrdev@qrdn.de>
2013-11-27 17:14:31 +0100

    'idle' and 'close' commands, revert keypress to external binary

--- mpdifier.py
+++ mpdifier.py
@@ -3,77 +3,18 @@
 import subprocess
 import mpdserver
 
-""" pressing media keys using WinAPI """
-#code from http://code.google.com/p/pywinauto/
-import ctypes
-from ctypes import *
-
-WORD = ctypes.c_ushort
-DWORD = ctypes.c_ulong
-
-class KEYBDINPUT(ctypes.Structure):
-    _pack_ = 2
-    _fields_ = [
-        ('wVk', WORD),
-        ('wScan', WORD),
-        ('dwFlags', DWORD),
-        ('time', DWORD),
-        ('dwExtraInfo', DWORD),
-    ]
-
-#dummys, don't need those structures here
-class MOUSEINPUT(ctypes.Structure):
-    pass
-
-class HARDWAREINPUT(ctypes.Structure):
-    pass
-
-class UNION_INPUT_STRUCTS(ctypes.Union):
-    _fields_ = [
-        ('mi', MOUSEINPUT),
-        ('ki', KEYBDINPUT),
-        ('hi', HARDWAREINPUT),
-    ]
-
-class INPUT(ctypes.Structure):
-    _pack_ = 2
-    _fields_ = [
-        ('type', DWORD),
-        ('_', UNION_INPUT_STRUCTS),
-    ]
-
-INPUT_KEYBOARD = 1
-KEYEVENTF_KEYUP = 2
-
-""" param  key - the key to press, must be a WORD """
 def pressKey(key):
-    inp = INPUT()
-    inp.type = INPUT_KEYBOARD
-    inp._.ki.wScan = 0
-    inp._.ki.time = 0
-    inp._.ki.dwExtraInfo = 0
-    inp._.ki.wVk = key
-    inp._.ki.dwFlags = 0
-
-    ctypes.windll.user32.SendInput(1,
-                                   ctypes.pointer(inp),
-                                   ctypes.sizeof(inp))
-
-    inp._.ki.dwFlags = KEYEVENTF_KEYUP
-
-    ctypes.windll.user32.SendInput(1,
-                                   ctypes.pointer(inp),
-                                   ctypes.sizeof(inp))
-
+    print "sending key %c" % key
+    subprocess.call(['sendMediaKeys.exe', key])
 
 class Key: # enum
-    VK_VOLUME_MUTE = 0xAD
-    VK_VOLUME_DOWN = 0xAE
-    VK_VOLUME_UP = 0xAF
-    VK_MEDIA_NEXT_TRACK = 0xB0
-    VK_MEDIA_PREV_TRACK = 0xB1
-    VK_MEDIA_STOP = 0xB2
-    VK_MEDIA_PLAY_PAUSE = 0xB3
+    VK_VOLUME_MUTE = "m"
+    VK_VOLUME_DOWN = "-"
+    VK_VOLUME_UP = "+"
+    VK_MEDIA_NEXT_TRACK = "n"
+    VK_MEDIA_PREV_TRACK = "p"
+    VK_MEDIA_STOP = "s"
+    VK_MEDIA_PLAY_PAUSE = "t"
 
 
 # command classes to be registered to mpd
@@ -83,23 +24,18 @@ class Status(mpdserver.CommandItems):
         return [("volume", 50)]
 
 class SetVol(mpdserver.Command):
+    muted = False # educated guess
     formatArg = [("vol", int)]
     def handle_args(self, **args):
         nv = args["vol"]
-        if nv < 50:
+        if self.muted or nv <= 0:
+            pressKey(Key.VK_VOLUME_MUTE)
+            self.muted = not self.muted #FIXME: seems not to work
+        elif nv < 50:
             pressKey(Key.VK_VOLUME_DOWN)
         else:
             pressKey(Key.VK_VOLUME_UP)
-        
 
-#class VolumeUp(mpdserver.Command):
-#    def handle_args(self, **args):
-#        pressKey(Key.VK_VOLUME_UP)
-#
-#class VolumeDown(mpdserver.Command):
-#    def handle_args(self, **args):
-#        pressKey(Key.VK_VOLUME_DOWN)
-#
 #class Mute(mpdserver.Command):
 #    def handle_args(self, **args):
 #        pressKey(Key.VK_VOLUME_MUTE)
@@ -125,23 +61,34 @@ class Stop(mpdserver.Command):
     def handle_args(self, **args):
         pressKey(Key.VK_MEDIA_STOP)
 
+class Idle(mpdserver.Command):
+    pass
+    
+class Close(mpdserver.Command):
+    #XXX: should close socket to client, but we have no access to that attribute here
+    pass
 
-""" setup & usage of mpdserver """
 def main():
-    mpd = mpdserver.MpdServer(6600)
-    mpd.requestHandler.RegisterCommand(mpdserver.Outputs)
+    """ setup & usage of mpdserver """
+    try:
+        mpd = mpdserver.MpdServer(6600)
+        mpd.requestHandler.RegisterCommand(mpdserver.Outputs)
 
-    mpd.requestHandler.RegisterCommand(SetVol)
+        mpd.requestHandler.RegisterCommand(SetVol)
 
-    mpd.requestHandler.RegisterCommand(Play)
-    mpd.requestHandler.RegisterCommand(Pause)
-    mpd.requestHandler.RegisterCommand(Next)
-    mpd.requestHandler.RegisterCommand(Previous)
-    mpd.requestHandler.RegisterCommand(Stop)
-    mpd.requestHandler.RegisterCommand(Status)
-    mpd.requestHandler.Playlist=mpdserver.MpdPlaylistDummy
+        mpd.requestHandler.RegisterCommand(Play)
+        mpd.requestHandler.RegisterCommand(Pause)
+        mpd.requestHandler.RegisterCommand(Next)
+        mpd.requestHandler.RegisterCommand(Previous)
+        mpd.requestHandler.RegisterCommand(Stop)
+        mpd.requestHandler.RegisterCommand(Status)
+        mpd.requestHandler._MpdRequestHandler__SupportedCommands['idle'] = { 'class':Idle, 'users':['default'], 'group':'read', 'mpdVersion':"0.14", 'neededBy':None }
+        mpd.requestHandler._MpdRequestHandler__SupportedCommands['close'] = { 'class':Idle, 'users':['default'], 'group':'read', 'mpdVersion':"0.14", 'neededBy':None }
+        mpd.requestHandler.Playlist=mpdserver.MpdPlaylistDummy
 
-    mpd.run()
+        mpd.run()
+    except KeyboardInterrupt:
+      return
 
 if __name__ == "__main__":
     main()
